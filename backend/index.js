ed4c57b986db545fe9f36deb3323065f8bd6e7a8
Ayan_khan_235 <babukhan640b@gmail.com>
2023-12-12 01:27:31 +0530

    post method

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app= express();
 const port =8080;
 
+// if you want to use post req you need to extend  without it alwyas undifined come
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+// if you use json format to send data 
+
+
+
 app.listen(port, (req,res)=>{
     console.log(`the port is  listening ont this ${port}`);
 });
@@ -9,10 +16,11 @@ app.listen(port, (req,res)=>{
 app.get("/register", (req,res)=>{
     let {user , password}=req.query;
    res.send(`Get standard method Welcone ${user}`);
-})
+});
 
 
 app.post("/register", (req,res)=>{
-    let {user , password}=req.query;
+    // let {user , password}=req.query;
+    let {user , password}=req.body;
    res.send(`Post  standard method Welcone ${user}`);
 })
